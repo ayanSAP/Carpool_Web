ae50cfbd01eaafd0b9103664332a67fa451795be
Ayan Abedin <125871144+ayanSAP@users.noreply.github.com>
2023-08-08 16:09:04 +0100

    Table creatd in driver-route

--- js/driver-route.js
+++ js/driver-route.js
@@ -2,39 +2,47 @@
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
-
-navBarToggle.addEventListener("click", function() {
+navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the form elements
-    var leavingFromInput = document.getElementById("leaving-from");
-    var goingToInput = document.getElementById("going-to");
-    var datetimeInput = document.getElementById("datetime");
-    var passengerCountInput = document.getElementById("passenger-count");
-    var searchButton = document.getElementById("search");
-  
-    // Add event listener to the search button
-    searchButton.addEventListener("click", function(event) {
-      event.preventDefault(); // Prevent form submission
-  
-      // Get the values entered by the user
-      var leavingFrom = leavingFromInput.value;
-      var goingTo = goingToInput.value;
-      var datetime = datetimeInput.value;
-      var passengerCount = passengerCountInput.value;
-  
-      // Perform any desired actions with the captured values
-      console.log("Leaving from:", leavingFrom);
-      console.log("Going to:", goingTo);
-      console.log("Datetime:", datetime);
-      console.log("Passenger count:", passengerCount);
-  
-      // Clear the form inputs
-      leavingFromInput.value = "";
-      goingToInput.value = "";
-      datetimeInput.value = "";
-      passengerCountInput.value = 1;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the form elements
+  var leavingFromInput = document.getElementById("leaving-from");
+  var goingToInput = document.getElementById("going-to");
+  var datetimeInput = document.getElementById("datetime");
+  var passengerCountInput = document.getElementById("passenger-count");
+  var searchButton = document.getElementById("search");
+
+  // Add event listener to the search button
+  searchButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the values entered by the user
+    var leavingFrom = leavingFromInput.value;
+    var goingTo = goingToInput.value;
+    var datetime = datetimeInput.value;
+    var passengerCount = passengerCountInput.value;
+
+    //storing the variables locally
+    window.localStorage.setItem("leaving-from", leavingFrom);
+    window.localStorage.setItem("going-to", goingTo);
+    window.localStorage.setItem("date-time", datetime);
+    window.localStorage.setItem("passenger-count", passengerCount);
+
+    //transferring the data to next page
+    window.location.href = "../found-ride.html";
+
+    // Perform any desired actions with the captured values
+    console.log("Leaving from:", leavingFrom);
+    console.log("Going to:", goingTo);
+    console.log("Datetime:", datetime);
+    console.log("Passenger count:", passengerCount);
+
+    // Clear the form inputs
+    leavingFromInput.value = "";
+    goingToInput.value = "";
+    datetimeInput.value = "";
+    passengerCountInput.value = 1;
   });
+});
