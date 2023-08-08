361b7178f582e513a70af07f0550ffd037d013dc
Ayan Abedin <125871144+ayanSAP@users.noreply.github.com>
2023-08-08 21:52:44 +0100

    driver route table created

--- js/driver-route.js
+++ js/driver-route.js
@@ -6,35 +6,43 @@ navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
 
-var list1 = [];
-var list2 = [];
-var list3 = [];
-var list4 = [];
+var searchButton = document.getElementById("search");
+searchButton.addEventListener("click", displayDetails);
 
-var n = 1;
-var x = 0;
+const form1 = document.getElementById("form_id");
 
-function AddRow() {
-  var AddRown = document.getElementById("show");
-  var NewRow = AddRow.insertRow(n);
+var row = 1;
 
-  list1[x] = document.getElementById("leaving-from").value;
-  list2[x] = document.getElementById("going-to").value;
-  list3[x] = document.getElementById("datetime").value;
-  list4[x] = document.getElementById("passenger-count").value;
+function displayDetails() {
+  var leavingFromInput = document.getElementById("leaving-from").value;
+  var goingToInput = document.getElementById("going-to").value;
+  var datetimeInput = document.getElementById("datetime").value;
+  var passengerCountInput = document.getElementById("passenger-count").value;
 
-  var cell1 = NewRow.insertCell(0);
-  var cell2 = NewRow.insertCell(1);
-  var cell3 = NewRow.insertCell(2);
-  var cell4 = NewRow.insertCell(3);
+  var display = document.getElementById("show");
 
-  cell1.innerHTML = list1[x];
-  cell2.innerHTML = list2[x];
-  cell3.innerHTML = list3[x];
-  cell4.innerHTML = list4[x];
+  var newRow = display.insertRow(row);
 
-  n++;
-  x++;
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+  var cell4 = newRow.insertCell(3);
+
+  cell1.innerHTML = leavingFromInput;
+  cell2.innerHTML = goingToInput;
+  cell3.innerHTML = datetimeInput;
+  cell4.innerHTML = passengerCountInput;
+
+  row++;
+
+  // Clear the form inputs
+  leavingFromInput.value = "";
+  goingToInput.value = "";
+  datetimeInput.value = "";
+  passengerCountInput.value = 1;
+
+  //reset the form
+  form1.reset();
 }
 
 // document.addEventListener("DOMContentLoaded", function () {
