fb1711efc2b405851cae5e85469289655aa72b34
Ayan Abedin <125871144+ayanSAP@users.noreply.github.com>
2023-08-09 14:17:13 +0100

    Firebase retrieve not working on page

--- js/driver-info.js
+++ js/driver-info.js
@@ -3,7 +3,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebas
 import {
   getDatabase,
   ref,
-  set,
   onValue,
   child,
   get,
@@ -31,64 +30,92 @@ const app = initializeApp(firebaseConfig);
 
 const dbRef = ref(getDatabase());
 
-function SelectAllData() {
-  get(child(dbRef, `drivers/`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        //   console.log(snapshot.val());
-        snapshot.forEach(function (currentRecord) {
-          var fullName = currentRecord.val().Full_Name;
-          var email = currentRecord.val().Email;
-          var contact = currentRecord.val().Email;
-          var details = currentRecord.val().Car_Details;
-          AddItemsToTable(fullName, email, contact, details);
-          console.log(currentRecord.val());
-        });
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
+// function SelectAllData() {
+//   get(child(dbRef, "drivers"))
+//     .then((snapshot) => {
+//       if (snapshot.exists()) {
+//         //   console.log(snapshot.val());
+//         var drivers = [];
+
+//         snapshot.forEach(function (childSnapshot) {
+//           drivers.push(childSnapshot.val());
+
+//           AddAllItemsToTable(drivers);
+
+//           //   var fullName = currentRecord.val().Full_Name;
+//           //   var email = currentRecord.val().Email;
+//           //   var contact = currentRecord.val().Contact_no;
+//           //   var details = currentRecord.val().Car_Details;
+//           //   AddItemsToTable(fullName, email, contact, details);
+//           //   console.log(currentRecord.val());
+//         });
+//       } else {
+//         console.log("No data available");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+//creating asysnc function
+async function fetchDataFromFirebase() {
+  try {
+    const dbRef = ref(getDatabase(), "drivers");
+    var drivers = [];
+    onValue(dbRef, (snapshot) => {
+      snapshot.forEach(function (childSnapshot) {
+        drivers.push(childSnapshot.val());
+        console.log(childSnapshot.val());
+
+        AddAllItemsToTable(drivers);
+      });
     });
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+    throw error; // Rethrow the error to be handled later if needed
+  }
 }
 
-window.onload = SelectAllData;
+function GetAllRealtime() {
+  try {
+    fetchDataFromFirebase();
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+    throw error; // Rethrow the error to be handled later if needed
+  }
+}
+
+window.onload = GetAllRealtime;
 
 // Filling The Table
+var tbody = document.getElementById("tbody1");
+
 function AddItemsToTable(fullName, email, contact, details) {
-  var tbody = document.getElementById("tbody1");
-  var trow = document.createElement("tr");
-  var td1 = document.createElement("td");
-  var td2 = document.createElement("td");
-  var td3 = document.createElement("td");
-  var td4 = document.createElement("td");
+  let trow = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  let td3 = document.createElement("td");
+  let td4 = document.createElement("td");
 
   td1.innerHTML = fullName;
   td2.innerHTML = email;
   td3.innerHTML = contact;
   td4.innerHTML = details;
 
-  console.log(td1, td2, td3, td4);
+  //   console.log(td1, td2, td3, td4);
 
   trow.appendChild(td1);
   trow.appendChild(td2);
   trow.appendChild(td3);
   trow.appendChild(td4);
+
   tbody.appendChild(trow);
 }
 
-// //   GET ALL DATA
-// function SelectAllData() {
-//   ref.on("value", function (AllRecords) {
-//     AllRecords.forEach(function (CurrentRecord) {
-//       var fullName = CurrentRecord.val().Full_Name;
-//       var email = CurrentRecord.val().Email;
-//       var contact = CurrentRecord.val().Email;
-//       var details = CurrentRecord.val().Car_Details;
-//       AddItemsToTable(fullName, email, contact, details);
-//     });
-//   });
-// }
-
-// window.onload = SelectAllData;
+function AddAllItemsToTable(TheDrivers) {
+  tbody.innerHTML = "";
+  TheDrivers.forEach((e) => {
+    AddItemsToTable(e.Full_Name, e.Email, e.Contact_no, e.Car_Details);
+  });
+}
