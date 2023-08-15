2a3ce68cd55943a8a30e73915c9287618e04007c
Ayan Abedin <125871144+ayanSAP@users.noreply.github.com>
2023-08-15 11:12:52 +0100

    Logo updated all page

--- js/index-next.js
+++ js/index-next.js
@@ -1,3 +1,12 @@
 const element = document.getElementById("change-user");
 element.innerHTML = "Hi, user";
 
+// Button 1
+function validate1() {
+  window.location = "../publish.html";
+}
+
+// Button 2
+function validate2() {
+  window.location = "../driver-route.html";
+}
